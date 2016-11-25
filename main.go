aa2aac8fa1379cfacd0a4f465e8cc37226c4c78f
Khoa Nguyen <kh.dang.nguyen@gmail.com>
2016-11-24 21:09:00 -0800

    Match song paths

--- main.go
+++ main.go
@@ -4,12 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func main() {
+	r := regexp.MustCompile(`<a class="audibleTile__artworkLink" href="(.*)">`)
+	i := 0
+
 	// Read lines from stdin
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		fmt.Println(s.Text())
+		line := s.Text()
+		match := r.FindStringSubmatch(line)
+		if len(match) == 0 {
+			continue
+		}
+		i++
+		fmt.Println(match[1])
 	}
+	fmt.Println("Total", i)
 }
