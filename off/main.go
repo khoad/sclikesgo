919445f52a43463483fa556b45e79169643febae
Khoa Nguyen <kh.dang.nguyen@gmail.com>
2016-11-28 20:38:56 -0800

    Skip downloaded songs

--- off/main.go
+++ off/main.go
@@ -21,21 +21,27 @@ func main() {
 			continue
 		}
 
-		// scurl := "https://soundcloud.com/pattycrashed/patty-crash-kronic-pictures"
-		offurl := off(scurl)
-
-		if offurl == "" {
-			fmt.Println("Failed", scurl)
-		}
-
 		rex := regexp.MustCompile(`https://soundcloud.com/(.*)`)
 		match := rex.FindStringSubmatch(scurl)
 		songName := strings.Replace(match[1], "/", "_", 1)
+		fullSongName := "/Users/knguyen/Desktop/sc/" + songName + ".mp3"
+
+		if _, err := os.Stat(fullSongName); err == nil {
+			fmt.Println("Song exists; skipping", fullSongName)
+			continue
+		}
+
+		// scurl := "https://soundcloud.com/pattycrashed/patty-crash-kronic-pictures"
+		offurl := off(scurl)
 
 		// offurl := "https://cf-media.sndcdn.com/p0kVgs7gZukF.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vcDBrVmdzN2dadWtGLjEyOC5tcDMiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE0ODAwODk3NDZ9fX1dfQ__&Signature=ohFuv~4FpKDVPV-vv7q-hsBFGDvLfu08A1Noa1Xr7NsI8BgbQsT6eCglwxw6UKEAwj8FPn4lbLFAKUNyhsEq-1cy8HWiYq0lPur207haDJPhavz83xHtqYyuezyZ4k5uZej8fI7ORND-ZnzcDAviXOfKrYQICCBZMMbcfjg5cZRtiJn2x3E4fOcFmKRsd~q70lc4R4j9hBEU8~9TgAQhaigmGYKmv2PAYvtQna76rZoHgStDMtwzyD3BuTRPlnKHS4q9d85ZL5AaGIT-oBixxg43S~ymTSYvkgqu~FMcaSLZD~Dzu-Ev8RSMZpFj~zR3orRUW-BY4NejiXbJ1-nWZA__&Key-Pair-Id=APKAJAGZ7VMH2PFPW6UQ"
-		songContent := download(offurl)
-
-		ioutil.WriteFile("/Users/knguyen/Desktop/sc/"+songName+".mp3", songContent, 0644)
+		if offurl == "" {
+			fmt.Println("Failed", scurl)
+			ioutil.WriteFile(fullSongName+".mp3.failed", []byte(""), 0644)
+		} else {
+			songContent := download(offurl)
+			ioutil.WriteFile(fullSongName, songContent, 0644)
+		}
 	}
 }
 
